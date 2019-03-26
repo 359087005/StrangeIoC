b87464204701b1e196a15ab6688352e801402a75
IceChou <359087005@qq.com>
2019-03-26 22:30:47 +0800

    文件重新读取

--- StrangeIoC/Assets/Audio/Editor/MyAudioEditor.cs
+++ StrangeIoC/Assets/Audio/Editor/MyAudioEditor.cs
@@ -1,26 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 
-public class AudioEditor : EditorWindow
+public class MyAudioEditor : EditorWindow
 {
-    [MenuItem("Tools/AudioEditor")]
+
+    private void Awake()
+    {
+        savePath = Application.dataPath + "\\Audio\\Resources\\AudioList.txt";
+
+        Debug.Log(Application.dataPath);
+
+        LoadAudioList();
+    }
+
+    [MenuItem("AudioEditor/AudioEditor")]
     static void CreatWindow()
     {
+        Debug.Log("CreatWindow successful.......");
         //Rect rect = new Rect(Screen.width / 2, Screen.height / 2, 400, 400);
         //AudioEditor window = EditorWindow.GetWindowWithRect(typeof(AudioEditor), rect) as AudioEditor;
-        AudioEditor window = EditorWindow.GetWindow<AudioEditor>("音效管理");
-
+        MyAudioEditor window = EditorWindow.GetWindow<MyAudioEditor>("音效管理....");
+        
         window.Show();
+        Debug.Log(window.name);
     }
 
     private string _name;
     private string _path;
 
     private Dictionary<string, string> audioDic = new Dictionary<string, string>();
+
     private void OnGUI()
     {
+        Debug.Log("AudioEditor OnGUI");
+
         if (audioDic.Count!= 0)
         {
             ShowAudioTitle();
@@ -45,7 +62,9 @@ public class AudioEditor : EditorWindow
                 return;
             }
             audioDic.Add(_name, _path);
+            SaveAudioList();
         }
+        AssetDatabase.Refresh();
     }
     /// <summary>
     /// 添加成功之后在最上方显示的信息菜单
@@ -78,7 +97,8 @@ public class AudioEditor : EditorWindow
             
             if (GUILayout.Button("删除", GUILayout.Width(100)))
             {
-                //DeleteAudioInfo(key);
+                DeleteAudioInfo(key);
+                return;
                 //TODO
             }
             GUILayout.EndHorizontal();
@@ -90,7 +110,45 @@ public class AudioEditor : EditorWindow
         if (audioDic.ContainsKey(key))
         {
             audioDic.Remove(key);
-            AssetDatabase.Refresh();
+        }
+    }
+
+
+    private string savePath = "";
+
+
+    /// <summary>
+    /// 音效列表信息保存
+    /// </summary>
+    private void SaveAudioList()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (string key in audioDic.Keys)
+        {
+            string value;
+            audioDic.TryGetValue(key,out value);
+
+            sb.Append(key + ","+value + "\n");
+        }
+       
+        File.WriteAllText(savePath, sb.ToString());
+    }
+
+
+    private void LoadAudioList()
+    {
+        if (!File.Exists(savePath))
+            return;
+        audioDic = new Dictionary<string, string>();
+        string[] allLines = File.ReadAllLines(savePath);
+
+        foreach (string line in allLines)
+        {
+            if (string.IsNullOrEmpty(line)) continue;
+
+            string[] keyValue = line.Split(',');
+
+            audioDic.Add(keyValue[0],keyValue[1]);
         }
     }
 }
