04a93034713906196db7d8e1886760e3f4aa5497
DKS-20160923UBY\Administrator <359087005@qq.com>
2019-03-27 17:38:32 +0800

    protobuf简单了解

---
+++ StrangeIoC/Assets/ProtoBuffNet/Scripts/MyUser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProtoBuf;
+
+/// <summary>
+/// 
+/// </summary>
+[ProtoContract]
+public class MyUser
+{
+    [ProtoMember(1)]
+    public int ID { get; set; }
+    [ProtoMember(2)]
+    public string UserName { get; set; }
+    [ProtoMember(3)]
+    public string PassWord { get; set; }
+    [ProtoMember(4)]
+    public int level { get; set; }
+    [ProtoMember(5)]
+    public UserType _UserType { get; set; }
+
+     public enum UserType
+    {
+        Master,
+        Warrior
+    }
+}
---
+++ StrangeIoC/Assets/ProtoBuffNet/Scripts/TestProtoBuf.cs
@@ -0,0 +1,51 @@
+﻿using ProtoBuf;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class TestProtoBuf : MonoBehaviour
+{
+    FileStream fs;
+    
+    void Start()
+    {
+        MyUser myUser = new MyUser();
+        myUser.ID = 1;
+        myUser.level = 100;
+        myUser.PassWord = "123456";
+        myUser.UserName = "zhb";
+        myUser._UserType = MyUser.UserType.Master;
+
+        using (fs = File.Create(Application.dataPath + "/User.bin"))
+        {
+            Serializer.Serialize<MyUser>(fs, myUser);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("???");
+            OpenRead();
+        }
+    }
+
+    void OpenRead()
+    {
+        MyUser user = null;
+        using (fs = File.OpenRead(Application.dataPath + "/User.bin"))
+        {
+            user = Serializer.Deserialize<MyUser>(fs);
+
+            Debug.Log(user.ID);
+            Debug.Log(user.UserName);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        AssetDatabase.Refresh();
+    }
+}
